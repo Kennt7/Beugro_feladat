226421b2884a9da831a7470491fcbe76f07bceae
Nagy Zoltán <zoli1984@gmail.hu>
2019-12-07 18:33:49 +0100

    Create customerorderinformation.sql

---
+++ database/customerorderinformation.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Gép: localhost
+-- Létrehozás ideje: 2019. Dec 07. 18:29
+-- Kiszolgáló verziója: 10.4.8-MariaDB
+-- PHP verzió: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `customerorderinformation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `customer`
+--
+
+CREATE TABLE `customer` (
+  `customerID` int(30) NOT NULL,
+  `firstName` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `lastName` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `address` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `phone` int(30) NOT NULL,
+  `fax` int(30) DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `customerorder`
+--
+
+CREATE TABLE `customerorder` (
+  `orderID` int(30) NOT NULL,
+  `orderDate` datetime NOT NULL,
+  `customerID` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `orderitem`
+--
+
+CREATE TABLE `orderitem` (
+  `catalogID` int(11) NOT NULL,
+  `orderID` int(30) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `color` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customerID`);
+
+--
+-- A tábla indexei `customerorder`
+--
+ALTER TABLE `customerorder`
+  ADD PRIMARY KEY (`orderID`),
+  ADD KEY `vasarloID` (`customerID`);
+
+--
+-- A tábla indexei `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD PRIMARY KEY (`catalogID`,`orderID`),
+  ADD KEY `rendelesID` (`orderID`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customerID` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `customerorder`
+--
+ALTER TABLE `customerorder`
+  MODIFY `orderID` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `orderitem`
+--
+ALTER TABLE `orderitem`
+  MODIFY `catalogID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `customerorder`
+--
+ALTER TABLE `customerorder`
+  ADD CONSTRAINT `customerorder_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerID`);
+
+--
+-- Megkötések a táblához `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD CONSTRAINT `orderitem_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `customerorder` (`orderID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
