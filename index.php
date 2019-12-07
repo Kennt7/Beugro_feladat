89eec44a05ded1a44cbdcc1f85b6da726b65078a
Nagy Zoltán <zoli1984@gmail.hu>
2019-12-07 19:37:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<?php header("Content-Type: text/html; charset=UTF-8"); ?>
+<?php require_once("adb_kat.inc.php")?>
+<html>
+    <meta name="author" content="Nagy Zoltán">
+	<head>
+		<title>Orders</title>
+	</head>
+	
+	<body>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<h1>
+			<legend>Customer orders</legend>
+		</h1>
+    
+		<div class="content">
+            
+      
+        <form action="" method="post">
+           
+            <div class="crudfields">
+            <input id="create" type="button" onclick="" value="Create"><br />
+                <br />
+            <input id="read" type="button" onclick="" value="Read"><br />
+                <br />
+            <input id="update" type="button" onclick="" value="Update"><br />
+                <br />
+            <input id="delete" type="button" onclick="" value="Delete"><br />
+                <br />
+            
+            
+            </div>
+            </form>
+    
+		</div>
+	</body>
+	
+<html>
