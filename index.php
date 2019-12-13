416480eb2b157c9c6c34c57a52803b068ced6632
Nagy Zoltán <zoli1984@gmail.hu>
2019-12-13 22:11:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,55 @@
 
-<?php header("Content-Type: text/html; charset=UTF-8"); ?>
 <?php require_once("adb_kat.php")?>
 <html>
     <meta name="author" content="Nagy Zoltán">
+    <meta charset="utf-8">
 	<head>
 		<title>Orders</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
 	</head>
 	
 	<body>
-		<link rel="stylesheet" type="text/css" href="css/index.css">
+		
 		<h1>
 			<legend>Customer orders</legend>
 		</h1>
     
-		<div class="content">
-            
-      
-        <form action="" method="post">
+		<div class=".container">
+            <div class=".col-md-4">
+            <table class="table table-dark table-hover">
+                <thead>
+                <tr>
+                <th>OrderId</th>
+                <th>orderDate</th>
+                <th>CustomerId</th>
+                </tr>
+                </thead>
+           <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+               
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+               
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+             </tbody>
+            </table>
+
+            <form action="" method="post">
            
             <div class="crudfields">
             <input id="create" type="button" onclick="" value="Create"><br />
@@ -26,11 +59,11 @@
             <input id="update" type="button" onclick="" value="Update"><br />
                 <br />
             <input id="delete" type="button" onclick="" value="Delete"><br />
-                <br />
-            
-            
+                <br />   
             </div>
             </form>
+               
+            </div>
     
 		</div>
 	</body>
