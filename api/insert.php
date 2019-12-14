ebfa80dfb2d7035909ecf98b3d1ffdeea15690cc
Nagy Zoltán <zoli1984@gmail.hu>
2019-12-14 21:50:40 +0100

    Update insert.php

--- api/insert.php
+++ api/insert.php
@@ -1,1 +1,21 @@
-<?php require_once("adb_kat.php")?>
+<?php 
+
+
+
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once("adb_kat.php");
+
+
+
+
+
+
+
+
+?>
