abe8703176cbfbe0e59708cb82deec95c5d8fe52
Nagy Zoltán <zoli1984@gmail.hu>
2020-01-03 20:03:32 +0100

    Update customerorderinformation.sql

--- database/customerorderinformation.sql
+++ database/customerorderinformation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: localhost
--- Létrehozás ideje: 2019. Dec 07. 20:51
+-- Létrehozás ideje: 2020. Jan 01. 21:09
 -- Kiszolgáló verziója: 10.4.8-MariaDB
 -- PHP verzió: 7.3.10
 
@@ -38,6 +38,15 @@ CREATE TABLE `customer` (
   `email` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `customer`
+--
+
+INSERT INTO `customer` (`customerID`, `firstName`, `lastName`, `address`, `phone`, `fax`, `email`) VALUES
+(1, 'Bögyös', 'Bianka', 'Bugyi', 703422171, NULL, 'bianka@hotmail.hu'),
+(2, 'Szőke', 'Sarolta', 'Debrecen', 301024159, 0, 'saroltaszoke@yahoo.com'),
+(3, 'Vörös', 'Veronika', 'Hollóháza', 203085627, NULL, 'veronika@gmail.com');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +58,15 @@ CREATE TABLE `customerorder` (
   `orderDate` datetime NOT NULL,
   `customerID` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `customerorder`
+--
+
+INSERT INTO `customerorder` (`orderID`, `orderDate`, `customerID`) VALUES
+(2, '2020-01-01 00:00:00', 1),
+(3, '2020-01-02 00:00:00', 2),
+(4, '2020-01-03 00:00:00', 3);
 
 -- --------------------------------------------------------
 
@@ -63,6 +81,15 @@ CREATE TABLE `product` (
   `color` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `product`
+--
+
+INSERT INTO `product` (`productID`, `orderID`, `amount`, `color`, `price`) VALUES
+(2, 2, 10, 'green', 800),
+(3, 3, 465, 'yelllow', 234),
+(4, 4, 5885, 'red', 1200);
 
 -- --------------------------------------------------------
 
@@ -115,19 +142,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customerID` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `customerID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT a táblához `customerorder`
 --
 ALTER TABLE `customerorder`
-  MODIFY `orderID` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `orderID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT a táblához `product`
 --
 ALTER TABLE `product`
-  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT a táblához `users`
